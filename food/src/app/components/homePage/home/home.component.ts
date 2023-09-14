76b09c7f390c57f31ccc32da4eee3d9a16a9c4ce
Alina <AliKochneva@yandex.ru>
2023-09-14 13:36:45 +0700

    filter

--- food/src/app/components/homePage/home/home.component.ts
+++ food/src/app/components/homePage/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FoodService } from '../../../services/food/food.service';
 import { Food } from 'src/app/shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,19 @@ import { Food } from 'src/app/shared/models/Food';
 export class HomeComponent {
 
   foods:Food[] = []
-  constructor(private foodService: FoodService){}
+  constructor(
+    private foodService: FoodService,
+    private route: ActivatedRoute
+  ){}
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll()
+    this.route.params.subscribe(params => {
+      if (params['searchTerm']){
+        this.foods = this.foodService.getAll().filter(food => food.name.toLocaleLowerCase().includes(params['searchTerm'].toLocaleLowerCase()))
+      } else {
+        this.foods = this.foodService.getAll()
+      }
+    })
   }
 
 }
