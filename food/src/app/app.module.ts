6499406e2759b78d1fbcbd944d4ee5ddf9f0a192
Alina <AliKochneva@yandex.ru>
2023-09-14 12:22:20 +0700

    header

--- food/src/app/app.module.ts
+++ food/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/homePage/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
