7bfcdd8ddfb98b18d0be36dabe19aba6310be166
Alina <AliKochneva@yandex.ru>
2023-09-15 16:19:52 +0700

    header

--- daily/src/app/app.module.ts
+++ daily/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/UI/logo/logo.component';
 import { SvgLogoComponent } from './components/SvgComponents/svg-logo/svg-logo.component';
+import { InputComponent } from './components/UI/input/input.component';
+import { ButtonComponent } from './components/UI/button/button.component';
+import { PenComponent } from './components/SvgComponents/pen/pen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SvgLogoComponent
+    SvgLogoComponent,
+    InputComponent,
+    ButtonComponent,
+    PenComponent
   ],
   imports: [
     BrowserModule,
--- daily/src/app/components/SvgComponents/svg-logo/svg-logo.component.ts
+++ daily/src/app/components/SvgComponents/svg-logo/svg-logo.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-svg-logo',
   templateUrl: './svg-logo.component.html',
-  styleUrls: ['./svg-logo.component.css']
 })
 export class SvgLogoComponent {
 
