a5ac95e9ad420bce611957fa95023a5c3ea3d475
Alina <AliKochneva@yandex.ru>
2023-09-15 11:49:52 +0700

    cart page

--- food/src/app/components/food-page/food-page.component.ts
+++ food/src/app/components/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart/cart.service';
 import { FoodService } from 'src/app/services/food/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -14,13 +15,20 @@ export class FoodPageComponent {
   food!:Food
   constructor (
     private activatedRoute: ActivatedRoute,
-    private foodService: FoodService
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
   ){
     activatedRoute.params.subscribe((params) => {
       if(params['id']){
         this.food = this.foodService.getFoodById(params['id'])
       }
     })
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page')
   }
 
 }
