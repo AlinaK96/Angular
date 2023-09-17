e066200746a519c33cc805aaa7749eb69375cdfe
Alina <AliKochneva@yandex.ru>
2023-09-17 15:13:40 +0700

    cards

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-card',
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 
 
-export class CardComponent {
+export class CardComponent implements OnInit {
 
+    title: string = 'Card title'
+    text: string = 'Sample text'
+    id: number = 26
+    disabled:boolean = true
+    imgUrl: string = 'https://sun9-31.userapi.com/impf/c636920/v636920407/4b104/qgWy1C1EpTw.jpg?size=320x320&quality=96&sign=a791819c977e127f10bf07cd63e6b753&c_uniq_tag=e2IkwMYaLgZQzIewMM_RNLJ351QUj6bFtNDWn3hfCq4&type=album'
+
+    ngOnInit(): void {
+        setTimeout(()=>{
+            this.imgUrl = 'https://edenhotelspa.com/v2/wp-content/uploads/2018/08/Blagaj-Mostar-BIH.jpg'
+            this.disabled = false
+        }, 2000)
+    }
+
+    changeTitle() {
+        this.title = 'New title'
+    }
+
+    inputHandler(event: any){
+        this.title = (event.target.value);
+        
+    }
+
+    inputHandler2(value: any){
+        this.title = (value);
+        
+    }
 }
