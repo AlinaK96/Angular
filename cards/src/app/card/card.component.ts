be7b16f0fb37557f4c5c473b06668c1708e93c77
Alina <AliKochneva@yandex.ru>
2023-09-17 15:32:44 +0700

    style binding

--- cards/src/app/card/card.component.ts
+++ cards/src/app/card/card.component.ts
@@ -14,6 +14,7 @@ export class CardComponent implements OnInit {
     id: number = 26
     disabled:boolean = true
     imgUrl: string = 'https://sun9-31.userapi.com/impf/c636920/v636920407/4b104/qgWy1C1EpTw.jpg?size=320x320&quality=96&sign=a791819c977e127f10bf07cd63e6b753&c_uniq_tag=e2IkwMYaLgZQzIewMM_RNLJ351QUj6bFtNDWn3hfCq4&type=album'
+    textColor: string = ''
 
     ngOnInit(): void {
         setTimeout(()=>{
@@ -26,13 +27,14 @@ export class CardComponent implements OnInit {
         this.title = 'New title'
     }
 
-    inputHandler(event: any){
-        this.title = (event.target.value);
-        
+    ChangeHandler() {
+        console.log(this.title);
     }
+    // inputHandler(event: any){
+    //     this.title = (event.target.value);
+    // }
 
-    inputHandler2(value: any){
-        this.title = (value);
-        
-    }
+    // inputHandler2(value: any){
+    //     this.title = (value);
+    // }
 }
